27a355610fe9baa9bf444738eb7f791c6e4be34c
trungdc-ha <dangchitrungha@gmail.com>
2021-05-06 20:49:53 +0700

    C11 Review callback, promise

--- src/app/student-info/student-info.component.ts
+++ src/app/student-info/student-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {Student} from "../model/Student";
 
 @Component({
@@ -6,21 +6,40 @@ import {Student} from "../model/Student";
   templateUrl: './student-info.component.html',
   styleUrls: ['./student-info.component.css']
 })
-export class StudentInfoComponent implements OnInit {
+export class StudentInfoComponent implements OnInit, OnChanges {
 
-  public student:Student = new Student(2,"Hong", 50);
+  public student: Student = new Student(2, "Hong", 50);
 
-  public isDisable:boolean = true;
+  public isDisable: boolean = true;
 
-  constructor() { }
+  public title: string = "Student info CP";
+
+  public color: string = "black";
+
+  constructor() {
+  }
 
   ngOnInit(): void {
-    if(this.student.point < 100){
+    if (this.student.point < 100) {
       this.isDisable = false;
     }
   }
 
+
   updatePoint(value: string) {
     this.student.point = parseInt(value);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(this.student.point);
+  }
+
+
+  update(value: string) {
+    if (value == "red") {
+      this.color = "red";
+    } else {
+      this.color = "black";
+    }
   }
 }
